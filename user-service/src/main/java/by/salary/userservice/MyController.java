946e882a84d95125b8e40ee29e75bf0ca898bf35
Denis Talalaev <71027278+DenisTalalaev@users.noreply.github.com>
2024-07-12 20:53:48 +0300

    Microservices init + Eureka

---
+++ user-service/src/main/java/by/salary/userservice/MyController.java
@@ -0,0 +1,14 @@
+package by.salary.userservice;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+}
