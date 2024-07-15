1e7a72577080629cb78b20250e6950ade6769193
Denis Talalaev <71027278+DenisTalalaev@users.noreply.github.com>
2024-07-15 23:28:00 +0300

    REST for user-service

---
+++ user-service/src/main/java/by/salary/userservice/controller/UserController.java
@@ -0,0 +1,17 @@
+package by.salary.userservice.controller;
+
+import by.salary.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+}
---
+++ user-service/src/main/java/by/salary/userservice/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package by.salary.userservice.repo;
+
+import by.salary.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
